c164a9763f24ed59bdc33a25939b9f529a7ab85e
joao-biondi <joaovitorantunesdossantos98@gmail.com>
2022-12-14 17:20:25 -0300

    first commit

---
+++ urna eletronica/etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '38111',
+        Nome:'joao biondi',
+        Partido:'PL',
+        fotos:[
+          {URL: 'cand.jpg', legenda: 'vereador'}
+        ]
+      },
+      {
+        numero: '77222',
+        Nome: 'emely eloise',
+        Partido: 'PL',
+        fotos:[
+          {URL:'cand5.jpg', legenda: 'vereador'}
+        ]
+      },
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: '2',
+    candidatos: [
+      {
+        numero: '99',
+        Nome: 'angel gabriel',
+        Partido: 'PL',
+        vice:'cc',
+        fotos:[
+          {URL:'cand5.jpg', legenda: 'prefeito'},
+          {URL:'cand6.jpg', legenda: 'vice-prefeito', small: true}
+        ]
+      },
+      {
+        numero: '84',
+        Nome: 'valdenir biondi',
+        Partido: 'PL',
+        vice: 'azul',
+        fotos:[
+          {URL:'cand6.jpg', legenda: 'prefeito'},
+          {URL:'cand7.jpg', legenda: 'vice-prefeito', small: true}
+        ]
+      }
+    ]
+  }
+]
+
+
